0823eb97b9baba7a80bd5358e9b88e0b69d12aeb
Ayushi Gupta <133566741+AyushiGupta160604@users.noreply.github.com>
2024-03-03 18:33:02 +0000

    updated summary

--- Basics_js/moreOnDatatypes.js
+++ Basics_js/moreOnDatatypes.js
@@ -12,6 +12,34 @@ let userEmail; // undefined
 const id = Symbol('123')
 const anotherId = Symbol('123')
 console.log(id == anotherId);
+const bigNumber = 34567813257998797098n; //last mein n makes it big int
 
 // Non primitive (Reference types)
 // Array, Objects, Functions
+const heros = ["Shaktimaan", "naagraj", "doga"];
+// {
+//     name: "ayushi",
+//     age: 22,
+// }
+// {} represents object [key value pair]
+// another way
+let myObj = {
+    name: "Ayushi",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber);
+//typeof function => function or function object
+// typeOf null(outsideTemp) => object
+// typeOf undefined => undefined
+// typeOf boolean => boolean;
+// typeOf number/ string =>number/ string
+//typeOf Object(native and does not implement [call]) => object
+//typeOf Object(native and does  implement [call]) => function
+// typeOf Symbol => Symbol
+
+// typeOf non primitive data types is object only and specially of function is function object
