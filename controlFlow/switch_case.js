a002a96c994ea2cb8911e528c7bfd8a4e7ff98e5
Ayushi Gupta <133566741+AyushiGupta160604@users.noreply.github.com>
2024-03-11 14:59:08 +0000

    control flow in js

---
+++ controlFlow/switch_case.js
@@ -0,0 +1,30 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+// if we remove break then it will execute all the statement below the matched statement without even checking except default case 
